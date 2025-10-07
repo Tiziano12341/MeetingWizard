8748094dcef5a26728147135f975c2eaf9bd7c26
Tiziano Avigliano <t.avigliano@perview.de>
2025-10-07 19:15:54 +0200

    add SecurityConfig class and therefore dependency

---
+++ src/main/java/de/training/SecurityConfig.java
@@ -0,0 +1,35 @@
+package de.training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());
+
+        return http.build();
+
+
+//Wie Vorschläge von allen verfügbaren methoden ohne KI
+        //Domain specfic languaes
+    }
+}
